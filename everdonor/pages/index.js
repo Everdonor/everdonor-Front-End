9575568721cb3a1822a36d4af104a96a4484a568
Tomas Hurrell <hurrelltomas@gmail.com>
2020-09-02 21:27:58 -0300

    Added pins to map

--- everdonor/pages/index.js
+++ everdonor/pages/index.js
@@ -1,34 +1,41 @@
-import React from "react";
-import { Component } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import React, {useEffect, useState} from "react";
+import ReactMapGL from 'react-map-gl';
+import Pins from '../components/Layout/pins';
+import EverdonorAPI from "../components/Layout/EverdonorAPI";
 
 const ApiKey = process.env.MAP_API
-debugger
-export default class Map extends Component {
 
-  state = {
-    viewport: {
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8
-    }
-  };
+export default function Map() {
 
-  render() {
-    return (
-      <div style={{ position: "relative" }}>
-        <ReactMapGL
-          {...this.state.viewport}
-          width='198vh'
-          height='89vh'
-          mapboxApiAccessToken={ApiKey}
-          onViewportChange={(viewport) => this.setState({ viewport })}
-        >
-          <Marker latitude={37.78} longitude={-122.41} offsetLeft={-20} offsetTop={-10}>
-            <div>You are here</div>
-          </Marker>
-        </ReactMapGL>
-      </div>
+    const [viewport, setViewport] = useState({
+            latitude: 37.7577,
+            longitude: -122.4376,
+            zoom: 8
+        }
     );
-  }
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        EverdonorAPI.getUsers('/users')
+            .then(resData => setUsers(resData))
+    }, []);
+
+    const _onClickMarker = () => {
+
+    };
+
+    return (
+        <div style={{position: "relative"}}>
+            <ReactMapGL
+                {...viewport}
+                width='198vh'
+                height='89vh'
+                mapboxApiAccessToken={ApiKey}
+                onViewportChange={(viewport) => setViewport(viewport)}
+            >
+                <Pins data={users} onClick={_onClickMarker}/>
+            </ReactMapGL>
+        </div>
+    );
+
 }
