302f26ce1af12f8ae9612b76086a9ddd21de07c0
Tobias <tobiascalvento@hotmail.com>
2020-09-20 22:16:58 -0300

    Added basic logic for login logic

--- everdonor/api-client/EverdonorAPI.js
+++ everdonor/api-client/EverdonorAPI.js
@@ -8,8 +8,9 @@ const API = {
     searchByType: (type) => Axios.get(`${server}users?type=${type}`).then(response => response.data),
     searchById: (id) => Axios.get(`${server}users/${id}`).then(response => response.data),
     createUser: (body) => Axios.post(`${server}sign-up`, { ...body }).then(response => response.data),
+    loginUser: (body) => Axios.post(`${server}login`, { ...body }).then(response => response),
     modifyUser: (body) => Axios.put(`${server}users/${body.id}`, { ...body }).then(response => response.data),
-    reportUser: (id) => Axios.put(`${server}users/${id}/report`).then(response => response.data).catch(error => {console.log(error)}),
+    reportUser: (id) => Axios.put(`${server}users/${id}/report`).then(response => response.data).catch(error => { console.log(error) }),
 };
 
 
